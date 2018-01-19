4e8ce3b1c73a205858c9ef09ea69a847daea6990
Edvin Basil <34038590+edvinbasil@users.noreply.github.com>
2018-01-19 21:56:08 +0530

    Update seedboxinit.sh

--- seedboxinit.sh
+++ seedboxinit.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-export HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
-export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
-curl https://duckdns.org/update/doedvin/7d50ccc9-87b3-42da-82e3-f0aaedbbce85/$(echo $PUBLIC_IPV4)
-echo Droplet: $HOSTNAME, IP Address: $PUBLIC_IPV4 > /var/www/html/ip.html
-echo success > /var/www/html/success.html
-su edvin
+curl "https://www.duckdns.org/update?domains=doedvin&token=7d50ccc9-87b3-42da-82e3-f0aaedbbce85&ip=" > /var/www/html/success.html
 deluge-web&
 deluged
