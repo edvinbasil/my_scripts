676e71f32b4ec9538c026cc7aad02bccca444548
Edvin Basil <34038590+edvinbasil@users.noreply.github.com>
2018-01-19 23:02:24 +0530

    Update seedboxinit.sh

--- seedboxinit.sh
+++ seedboxinit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "<html><head><link rel="icon" type="image/x-icon" href="https://protoface.com/favicon.png"></head<body><a href="./upload">Add New</a><br><a href="./Downloads">View Downloaded</a><br><a href="http://edvin.cf:8112">GUI</a></body></html>" > /var/www/html/index.html
+echo "<html><head><link rel="icon" type="image/x-icon" href="#"></head<body><a href="./upload">Add New</a><br><a href="./Downloads">View Downloaded</a><br><a href="http://edvin.cf:8112">GUI</a></body></html>" > /var/www/html/index.html
 /home/edvin/curl.sh
 deluge-web&
 deluged
