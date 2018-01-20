62bc0eb3fcd2ab3d3483a40e711e59b6f2a214f0
Edvin Basil <34038590+edvinbasil@users.noreply.github.com>
2018-01-20 13:35:32 +0530

    Update interact.php

--- interact.php
+++ interact.php
@@ -1,9 +1,22 @@
+<?php
+    if (isset($_POST['button']))
+    {
+         $equery = "rclone sync /var/www/html/Downloads/ g:rcloned/Downloads ";
+         $out = exec("$equery");
+         echo $out;
+    }
+?>
 <html>
-<head>
-<link rel="icon" type="image/x-icon" href="https://protoface.com/favicon.png">
-</head>
-<body>
-<a href="./upload">Add New</a><br><a href="./Downloads">View Downloaded</a><br>
-<a href="http://edvin.cf:8112">GUI</a>
-</body>
+  <head>
+    <link rel="icon" type="image/x-icon" href="https://protoface.com/favicon.png">
+  </head>
+  <body>
+    <a href="./upload">Add New</a><br><a href="./Downloads">View Downloaded</a><br>
+    <a href="http://edvin.cf:8112">GUI</a><br>
+    <form method="post">
+    <p>
+        <button name="button">RcloneCopy</button>
+    </p>
+    </form>
+  </body>
 </html>
